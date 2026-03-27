e12207261098f8d0951371bef128414760961cbb
Ashirvad Mohanty <Ashirvad.Mohanty@hsc.com>
2026-03-27 12:00:15 +0000

    feat: add clang-tidy check for guideline HSCAI.2.5

--- AITwoFiveCheck.cpp
+++ AITwoFiveCheck.cpp
@@ -14,19 +14,67 @@ using namespace clang::ast_matchers;
 namespace clang::tidy::hsc {
 
 void AITwoFiveCheck::registerMatchers(MatchFinder *Finder) {
-  // FIXME: Add matchers.
-  Finder->addMatcher(functionDecl().bind("x"), this);
+  Finder->addMatcher(
+            cxxReinterpretCastExpr().bind("reinterpretCast"),
+            this);
+}
+
+static bool isAllowedTargetType(const QualType Target, ASTContext &Ctx) {
+    if (Target->isVoidType() || Target->isCharType() || 
+            (Target->isUnsignedIntegerType() && Ctx.getTypeSize(Target) == 8))
+        return true;
+
+    if (const auto *ET = Target->getAs<EnumType>()) {
+        if (ET->getDecl()->getName() == "byte" && ET->getDecl()->getQualifiedNameAsString() == "std::byte")
+            return true;
+    }
+
+    if (Target->isIntegerType()) {
+        const auto *BT = Target->getAs<BuiltinType>();
+        if (BT && (BT->getKind() == BuiltinType::ULongLong ||
+                    BT->getKind() == BuiltinType::UInt128 ||
+                    BT->getKind() == BuiltinType::ULong ||
+                    (Ctx.getTypeSize(Target) >= Ctx.getTypeSize(Ctx.VoidPtrTy))))
+            return true;
+    }
+    return false;
+}
+
+static bool isAllowedPointerTarget(const QualType Target, ASTContext &Ctx) {
+    QualType Pointee = Target->getPointeeType();
+    if (Pointee.isNull())
+        return false;
+
+    Pointee = Pointee.getCanonicalType().getUnqualifiedType();
+
+    if (Pointee->isVoidType() || Pointee->isCharType() 
+            || (Pointee->isUnsignedIntegerType() && Ctx.getTypeSize(Pointee) == 8))
+        return true;
+
+    if (const auto *ET = Pointee->getAs<EnumType>()) {
+        if (ET->getDecl()->getName() == "byte" &&
+                ET->getDecl()->getQualifiedNameAsString() == "std::byte")
+            return true;
+    }
+
+    return false;
 }
 
 void AITwoFiveCheck::check(const MatchFinder::MatchResult &Result) {
-  // FIXME: Add callback implementation.
-  const auto *MatchedDecl = Result.Nodes.getNodeAs<FunctionDecl>("x");
-  if (!MatchedDecl->getIdentifier() || MatchedDecl->getName().starts_with("awesome_"))
-    return;
-  diag(MatchedDecl->getLocation(), "function %0 is insufficiently awesome")
-      << MatchedDecl
-      << FixItHint::CreateInsertion(MatchedDecl->getLocation(), "awesome_");
-  diag(MatchedDecl->getLocation(), "insert 'awesome'", DiagnosticIDs::Note);
+  const auto *Cast = Result.Nodes.getNodeAs<CXXReinterpretCastExpr>("reinterpretCast");
+  if (!Cast)
+      return;
+
+  QualType TargetType = Cast->getType().getCanonicalType();
+  QualType SourceType = Cast->getSubExpr()->getType().getCanonicalType();
+
+  if (TargetType->isPointerType() && isAllowedPointerTarget(TargetType, *Result.Context))
+      return;
+
+  if (TargetType->isIntegerType() && SourceType->isPointerType() && isAllowedTargetType(TargetType, *Result.Context))
+      return;
+
+  diag(Cast->getBeginLoc(), "reinterpret_cast shall not be used between unrelated types") << FixItHint();
 }
 
 } // namespace clang::tidy::hsc
