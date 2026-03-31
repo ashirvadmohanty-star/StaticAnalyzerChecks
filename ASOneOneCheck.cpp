72a76ef0769289da50457dd79a9672eb2e6bcdbe
Ashirvad Mohanty <Ashirvad.Mohanty@hsc.com>
2026-03-31 08:42:56 +0000

    feat: add clang-tidy check for guideline HSCAS.1.1

--- ASOneOneCheck.cpp
+++ ASOneOneCheck.cpp
@@ -1,12 +1,12 @@
 //===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
 
 #include "ASOneOneCheck.h"
+#include "clang/AST/Expr.h"
 #include "clang/ASTMatchers/ASTMatchFinder.h"
 
 using namespace clang::ast_matchers;
@@ -14,19 +14,30 @@ using namespace clang::ast_matchers;
 namespace clang::tidy::hsc {
 
 void ASOneOneCheck::registerMatchers(MatchFinder *Finder) {
-  // FIXME: Add matchers.
-  Finder->addMatcher(functionDecl().bind("x"), this);
+  Finder->addMatcher(
+      cxxThrowExpr().bind("throwExpr"),
+      this);
 }
 
 void ASOneOneCheck::check(const MatchFinder::MatchResult &Result) {
-  // FIXME: Add callback implementation.
-  const auto *MatchedDecl = Result.Nodes.getNodeAs<FunctionDecl>("x");
-  if (!MatchedDecl->getIdentifier() || MatchedDecl->getName().starts_with("awesome_"))
+  const auto *Throw = Result.Nodes.getNodeAs<CXXThrowExpr>("throwExpr");
+  if (!Throw)
     return;
-  diag(MatchedDecl->getLocation(), "function %0 is insufficiently awesome")
-      << MatchedDecl
-      << FixItHint::CreateInsertion(MatchedDecl->getLocation(), "awesome_");
-  diag(MatchedDecl->getLocation(), "insert 'awesome'", DiagnosticIDs::Note);
+
+  const Expr *SubExpr = Throw->getSubExpr();
+
+  // Skip rethrow (throw;)
+  if (!SubExpr)
+    return;
+
+  QualType QT = SubExpr->getType();
+
+  // Check if thrown type is pointer
+  if (!QT->isPointerType())
+    return;
+
+  diag(Throw->getExprLoc(),
+       "exception object shall not have pointer type");
 }
 
 } // namespace clang::tidy::hsc
