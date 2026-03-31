65025b0a90201e8093a6f245198911aa7dcf7b18
Ashirvad Mohanty <Ashirvad.Mohanty@hsc.com>
2026-03-31 08:48:01 +0000

    feat: add clang-tidy check for guideline HSCAS.4.1

--- ASFourOneCheck.cpp
+++ ASFourOneCheck.cpp
@@ -1,12 +1,12 @@
 //===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
 
 #include "ASFourOneCheck.h"
+#include "clang/AST/DeclCXX.h"
 #include "clang/ASTMatchers/ASTMatchFinder.h"
 
 using namespace clang::ast_matchers;
@@ -14,19 +14,53 @@ using namespace clang::ast_matchers;
 namespace clang::tidy::hsc {
 
 void ASFourOneCheck::registerMatchers(MatchFinder *Finder) {
-  // FIXME: Add matchers.
-  Finder->addMatcher(functionDecl().bind("x"), this);
+  Finder->addMatcher(
+      functionDecl(
+          anyOf(
+              cxxDestructorDecl(),
+              cxxConstructorDecl(isMoveConstructor()),
+              cxxMethodDecl(isMoveAssignmentOperator())
+          ),
+          unless(isNoThrow())
+      ).bind("func"),
+      this);
 }
 
 void ASFourOneCheck::check(const MatchFinder::MatchResult &Result) {
-  // FIXME: Add callback implementation.
-  const auto *MatchedDecl = Result.Nodes.getNodeAs<FunctionDecl>("x");
-  if (!MatchedDecl->getIdentifier() || MatchedDecl->getName().starts_with("awesome_"))
+  const auto *FD = Result.Nodes.getNodeAs<FunctionDecl>("func");
+  if (!FD)
     return;
-  diag(MatchedDecl->getLocation(), "function %0 is insufficiently awesome")
-      << MatchedDecl
-      << FixItHint::CreateInsertion(MatchedDecl->getLocation(), "awesome_");
-  diag(MatchedDecl->getLocation(), "insert 'awesome'", DiagnosticIDs::Note);
+
+  // Avoid implicit functions (compiler-generated)
+  if (FD->isImplicit())
+    return;
+
+  // Get readable function kind
+  std::string FuncType = "function";
+
+  if (isa<CXXDestructorDecl>(FD))
+    FuncType = "destructor";
+  else if (const auto *Ctor = dyn_cast<CXXConstructorDecl>(FD)) {
+    if (Ctor->isMoveConstructor())
+      FuncType = "move constructor";
+  } else if (const auto *Method = dyn_cast<CXXMethodDecl>(FD)) {
+    if (Method->isMoveAssignmentOperator())
+      FuncType = "move assignment operator";
+  }
+
+  diag(FD->getLocation(),
+       "%0 should be marked noexcept")
+      << FuncType;
+
+  // Optional FixIt (basic)
+  if (FD->getTypeSourceInfo()) {
+    SourceLocation EndLoc = FD->getEndLoc();
+
+    diag(FD->getLocation(),
+         "add 'noexcept' specifier",
+         DiagnosticIDs::Note)
+        << FixItHint::CreateInsertion(EndLoc, " noexcept");
+  }
 }
 
 } // namespace clang::tidy::hsc
